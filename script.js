c08ddd498b685869f52b0aa9e446803105beeda4
ewdwins <saleh.tarkashvand@gmail.com>
2023-08-17 14:46:44 +0330

    end search box

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const body = document.querySelector("body"),
-    modeToggle = body.querySelector(".mode-toggle")
+const body = document.querySelector("body") ,
+    modeToggle = body.querySelector(".mode-toggle") , 
+    sidebarToggle = document.querySelector(".sidebar-toggle") ,
+    sidebar = document.querySelector("nav")
 
 modeToggle.addEventListener("click" , ()=>{
     body.classList.toggle("dark")
 })
+
+sidebarToggle.addEventListener("click" , ()=>{
+    sidebar.classList.toggle("close")
+})
+
+
+
+window.addEventListener('resize', ()=>{
+
+    if (window.innerWidth > 376) {
+        // Then log this message to the console
+        sidebar.classList.toggle("close")
+      } else {
+        sidebar.classList.toggle("close")
+      }
+
+});
